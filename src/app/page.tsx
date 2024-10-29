94b0ecf9bcdb6c75dad59ef687e33199b8b51c72
hvkshetry <hvkshetry@gmail.com>
2024-10-29 16:28:28 -0400

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import LimeSoftenerCalculator from '@/components/LimeSoftenerCalculator'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-4">
+      <LimeSoftenerCalculator />
+    </main>
+  )
+}
