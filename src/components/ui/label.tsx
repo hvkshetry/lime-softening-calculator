f1837d44b93766f1de1f6ae2b036a57d5f10b547
hvkshetry <hvkshetry@gmail.com>
2024-10-29 20:55:54 -0400

    Create label.tsx

---
+++ src/components/ui/label.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import * as React from "react"
+
+const Label = React.forwardRef<
+  HTMLLabelElement,
+  React.LabelHTMLAttributes
+>(({ className, ...props }, ref) => (
+  
+))
+Label.displayName = "Label"
+
+export { Label }
