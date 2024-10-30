d040486f859c1996e88fcbd9ab5a3d75f0c58d69
hvkshetry <hvkshetry@gmail.com>
2024-10-29 20:55:30 -0400

    Create input.tsx

---
+++ src/components/ui/input.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import * as React from "react"
+
+const Input = React.forwardRef<
+  HTMLInputElement,
+  React.InputHTMLAttributes
+>(({ className, type, ...props }, ref) => {
+  return (
+    
+  )
+})
+Input.displayName = "Input"
+
+export { Input }
