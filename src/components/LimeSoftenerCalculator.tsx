ff89410d189e4e8ccfb94b0cbde8153dc1f8bfeb
hvkshetry <hvkshetry@gmail.com>
2024-10-29 20:54:03 -0400

    Update LimeSoftenerCalculator.tsx

--- src/components/LimeSoftenerCalculator.tsx
+++ src/components/LimeSoftenerCalculator.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+'use client';
+
 import React, { useState, useEffect } from 'react';
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
