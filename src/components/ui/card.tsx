f4a5867107cf1a07bbeb26e35e84ea9dc5e94b99
hvkshetry <hvkshetry@gmail.com>
2024-10-29 21:14:29 -0400

    Update card.tsx

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -2,36 +2,56 @@
 
 import * as React from "react"
 
-const Card = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes
->(({ className, ...props }, ref) => (
-  
-))
+const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className="rounded-lg border bg-white shadow-sm"
+        {...props}
+      />
+    )
+  }
+)
 Card.displayName = "Card"
 
-const CardHeader = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes
->(({ className, ...props }, ref) => (
-  
-))
+const CardHeader = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className="flex flex-col space-y-1.5 p-6"
+        {...props}
+      />
+    )
+  }
+)
 CardHeader.displayName = "CardHeader"
 
-const CardTitle = React.forwardRef<
-  HTMLParagraphElement,
-  React.HTMLAttributes
->(({ className, ...props }, ref) => (
-  
-))
+const CardTitle = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLHeadingElement>>(
+  ({ className, ...props }, ref) => {
+    return (
+      <h3
+        ref={ref}
+        className="text-2xl font-semibold leading-none tracking-tight"
+        {...props}
+      />
+    )
+  }
+)
 CardTitle.displayName = "CardTitle"
 
-const CardContent = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes
->(({ className, ...props }, ref) => (
-  
-))
+const CardContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => {
+    return (
+      <div 
+        ref={ref} 
+        className="p-6 pt-0" 
+        {...props} 
+      />
+    )
+  }
+)
 CardContent.displayName = "CardContent"
 
 export { Card, CardHeader, CardTitle, CardContent }
